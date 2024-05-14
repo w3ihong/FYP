19145226e3d412b354706c47e4d2934fe19cdcf4
Wei_H0ng <weihongchew2@gmail.com>
2024-05-14 16:58:25 +0800

    added main structure

--- app/notes/page.tsx
+++
@@ -1,9 +0,0 @@
-import FetchDataSteps from '@/components/tutorial/FetchDataSteps'
-import { createClient } from '@/utils/supabase/server'
-
-export default async function Page() {
-  const supabase = createClient()
-  const { data: user_reports } = await supabase.from('user_reports').select()
-
-  return <pre>{JSON.stringify(user_reports, null, 2)}</pre>
-}
