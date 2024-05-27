a3a43c83b3b2005a4bbf94ca3a1c0d0fa79f62b0
Wei_H0ng <weihongchew2@gmail.com>
2024-05-27 17:38:00 +0800

    added default pages to admin reports and admin suspend

---
+++ app/admin/reports/page.tsx
@@ -0,0 +1,8 @@
+export default function adminReports() {
+    return (
+      <div>
+        <h1>admin reports page</h1>
+        
+      </div>
+    );
+}
---
+++ app/admin/suspend/page.tsx
@@ -0,0 +1,8 @@
+export default function adminSuspend() {
+    return (
+      <div>
+        <h1>admin suspend page</h1>
+        
+      </div>
+    );
+}
