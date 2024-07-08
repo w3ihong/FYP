f11e16f8214c76a4cdeef135f6acec9efa029eae
Calista Bianca <calistabnca@gmail.com>
2024-07-08 15:15:37 +0700

    made changes to analytics folder for analsysis dashboard

--- app/protected/analytics/layout.tsx
+++ app/protected/analytics/layout.tsx
@@ -8,7 +8,7 @@ interface DashboardLayoutProps {
 const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Analysis Dashboard</h1>
+      <h1 className="text-2xl font-bold mb-8">Analysis Dashboard</h1>
       <div className="grid grid-cols-3 gap-4">
         {children}
       </div>
