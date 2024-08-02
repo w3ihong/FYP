0c231dd7d204b0785cbc2976860e6258c12e02e6
Calista Bianca <calistabnca@gmail.com>
2024-08-02 14:25:22 +0700

    homepage UI changes

--- app/landing/pricing.tsx
+++ app/landing/pricing.tsx
@@ -1,99 +1,134 @@
-"use server"
+"use client";
 
-import React from 'react';
+import React, { useRef } from 'react';
+import { motion, useInView } from 'framer-motion';
+import { twMerge } from "tailwind-merge";
+
+const pricingTiers = [
+  {
+    title: "Free",
+    monthlyPrice: 0,
+    buttonText: "Get started for free",
+    inverse: false,
+    features: [
+      "Small-scale metrics",
+      "Basic Support",
+    ],
+  },
+  {
+    title: "Pro",
+    monthlyPrice: 39,
+    buttonText: "Sign up now",
+    inverse: true,
+    features: [
+      "Visual Analytics",
+      "Sentiment Analysis",
+      "Trend Analysis",
+      "Comparative Analysis",
+    ]
+  }
+]
 
 const Pricing = () => {
-  return (
-    <section id="pricing">
-      <div className="max-w-screen-xl px-4 py-8 mx-auto lg:py-28 lg:px-6">
-        <div className="max-w-screen-md mx-auto mb-8 text-center lg:mb-12">
-          <h2 className="mb-4 text-3xl font-raleway font-bold tracking-tight text-accent">
-            Designed for individuals like you
-          </h2>
-          <p className="mb-5 font-light text-gray-600 sm:text-xl">
-            Here at EchoSphere, we analyze your data to produce maximum results.
-          </p>
-        </div>
+  const headingRef = useRef(null);
+  const isHeadingInView = useInView(headingRef, { once: true });
+  const cardRef = useRef(null);
+  const isInView = useInView(cardRef, { once: true });
 
-        <div className="flex flex-col lg:flex-row justify-center lg:space-x-6 sm:gap-6 xl:gap-10">
-          {/* Pricing Card - Free */}
-          <div className="flex flex-col p-6 mx-auto mb-8 text-center text-gray-900 bg-white border border-gray-100 rounded-lg shadow h-[490px] lg:w-1/2 w-full">
-            <div>
-              <h3 className="mt-2 mb-2 text-2xl font-raleway font-semibold">Free</h3>
-              <p className="font-light text-gray-500 sm:text-lg">
-                Best option for personal use
-              </p>
-              <div className="flex items-baseline justify-center my-8 mb-10">
-                <span className="mr-2 text-5xl font-extrabold">$0</span>
-                <span className="text-gray-500">/month</span>
+  return (
+    <section id="pricing" className="py-24">
+      <div className="container">
+        <motion.h2 
+          ref={headingRef}
+          initial={{ opacity: 0, y: 20 }}
+          animate={isHeadingInView ? { opacity: 1, y: 0 } : {}}
+          transition={{ duration: 0.8 }}
+          className="section-title"
+        >
+          Pricing
+        </motion.h2>
+
+        <motion.p 
+          ref={headingRef}
+          initial={{ opacity: 0, y: 20 }}
+          animate={isHeadingInView ? { opacity: 1, y: 0 } : {}}
+          transition={{ duration: 0.8 }}
+          className="section-description"
+        >
+          Free forever. Upgrade for exclusive features.
+        </motion.p>
+
+        <motion.div
+          ref={cardRef}
+          initial={{ opacity: 0, y: 20 }}
+          animate={isInView ? { opacity: 1, y: 0 } : {}}
+          transition={{ duration: 0.8 }}
+          className="flex flex-col gap-6 items-center mt-10 lg:flex-row lg:items-end lg:justify-center"
+        >
+          {pricingTiers.map(({ title, monthlyPrice, buttonText, inverse, features }) => (
+            <motion.div
+              key={title}
+              className={twMerge(
+                "card",
+                inverse && 'border-accent bg-accent text-white'
+              )}
+              whileHover={{ 
+                scale: 1.03, 
+                boxShadow: '0 8px 16px rgba(0, 0, 0, 0.15)' 
+              }}
+              transition={{ 
+                type: 'tween', 
+                duration: 0.3 
+              }}
+            >
+              <h3 className={twMerge(
+                "text-lg font-bold text-accent",
+                inverse && 'text-white'
+              )}>
+                {title}
+              </h3>
+              <div className="flex items-baseline gap-1 mt-[30px]">
+                <span className="text-4xl font-bold tracking-tighter leading-none">
+                  ${monthlyPrice}
+                </span>
+                <span className={twMerge(
+                  'tracking-light font-bold text-black/50',
+                  inverse && 'text-white/50'
+                )}>
+                  SGD/month
+                </span>
               </div>
-            </div>
-            <div className="flex-grow">
-              {/* List */}
-              <ul role="list" className="mb-8 space-y-4 text-left">
-                <li className="flex items-center space-x-3">
-                  {/* Icon */}
-                  <svg className="flex-shrink-0 w-5 h-5 text-green-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
-                  </svg>
-                  <span>Small-scale Metrics</span>
-                </li>
+
+              <button className={twMerge(
+                "text-white bg-accent hover:bg-blue-800 font-bold font-raleway rounded-lg text-sm w-full py-2.5 me-2 mb-2 mt-[30px]",
+                inverse && 'text-accent bg-white hover:bg-gray-400'
+              )}>
+                {buttonText}
+              </button>
+
+              <ul className="flex flex-col gap-5 mt-8">
+                {features.map(feature => (
+                  <li key={feature} className="text-sm flex items-center gap-4">
+                    <svg 
+                      xmlns="http://www.w3.org/2000/svg" 
+                      fill="none" viewBox="0 0 24 24" 
+                      strokeWidth="1.5" stroke="currentColor" 
+                      className="size-6"
+                    >
+                      <path 
+                        strokeLinecap="round" 
+                        strokeLinejoin="round" 
+                        d="m4.5 12.75 6 6 9-13.5" 
+                      />
+                    </svg>
+                    <span>{feature}</span>
+                  </li>
+                ))}
               </ul>
-            </div>
-            <div>
-              <a href="#" className="text-white bg-accent hover:bg-SBaccent font-raleway font-medium rounded-lg border-2 border-gray-700 text-sm px-20 py-2.5 mb-10 text-center">Get started</a>
-            </div>
-          </div>
-          {/* Pricing Card - Premium */}
-          <div className="flex flex-col p-6 mx-auto text-center text-gray-900 bg-white border border-gray-100 rounded-lg shadow h-[490px] lg:w-1/2 w-full">
-            <div>
-              <h3 className="mb-2 mt-2 text-2xl font-raleway font-semibold">Premium</h3>
-              <p className="font-light text-gray-500 sm:text-lg">
-                Relevant for social media managers, small businesses
-              </p>
-              <div className="flex items-baseline justify-center my-8 mb-10">
-                <span className="mr-2 text-5xl font-extrabold">$99</span>
-                <span className="text-gray-500">/month</span>
-              </div>
-            </div>
-            <div className="flex-grow">
-              {/* List */}
-              <ul role="list" className="mb-8 space-y-4 text-left">
-                <li className="flex items-center space-x-3">
-                  {/* Icon */}
-                  <svg className="flex-shrink-0 w-5 h-5 text-green-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
-                  </svg>
-                  <span>In-depth Visual Insights</span>
-                </li>
-                <li className="flex items-center space-x-3">
-                  {/* Icon */}
-                  <svg className="flex-shrink-0 w-5 h-5 text-green-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
-                  </svg>
-                  <span>Competitive Analysis</span>
-                </li>
-                <li className="flex items-center space-x-3">
-                  {/* Icon */}
-                  <svg className="flex-shrink-0 w-5 h-5 text-green-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
-                  </svg>
-                  <span>Sentiment Analysis</span>
-                </li>
-                <li className="flex items-center space-x-3">
-                  {/* Icon */}
-                  <svg className="flex-shrink-0 w-5 h-5 text-green-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
-                  </svg>
-                  <span>Trend Indicator</span>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <a href="#" className="text-white bg-accent hover:bg-SBaccent font-raleway font-medium rounded-lg border-2 border-gray-700 text-sm px-20 py-2.5 mb-10 text-center">Get started</a>
-            </div>
-          </div>
-        </div>
+            </motion.div>
+          ))}
+        </motion.div>
+
       </div>
     </section>
   );
