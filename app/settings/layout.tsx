f99d6732675c89111299bb2ca40b2789ec2287b6
Calista Bianca <calistabnca@gmail.com>
2024-08-02 14:22:32 +0700

    changed UI for account settings (still need to do error handling)

--- app/settings/layout.tsx
+++ app/settings/layout.tsx
@@ -3,33 +3,32 @@ import Sidebar from '@/components/sidebarSettings';
 import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
 
-
 export default async function ProtectedLayout({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
-    const supabase = createClient();  
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const supabase = createClient();  
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-    console.log(user?.id);
-    
-    // comment out to aid with development
-     if (!user) {
-       console.log('User not found');
-       return redirect("../../landing/login");
-     }
+  console.log(user?.id);
+  
+  // comment out to aid with development
+  if (!user) {
+    console.log('User not found');
+    return redirect("../../landing/login");
+  }
 
-    return (
-        <div className='flex flex-row w-full min-h-screen'>
-            <Sidebar/>
-            <div className="h-full w-16">
-            </div>
-            <div className="flex-1 w-full flex flex-col gap-6 p-6">
-                {children}
-            </div>
-        </div>
-    );
+  return (
+    <div className='flex'>
+    <div>
+      <Sidebar/>
+    </div>
+      <div className="flex-1">
+        {children}
+      </div>
+    </div>
+  );
 }
